8457a2751dca85f7dd47dac7462cc4a354fb9ee1
NicholusMagak <43399149+NicholusMagak@users.noreply.github.com>
2019-11-11 14:50:18 +0300

    Update ANN_assignment.py

--- ANN_assignment.py
+++ ANN_assignment.py
@@ -29,14 +29,20 @@ Norm_outputs = scalar.transform(outputs)
 
 print(Norm_outputs, "\n")
 
+#learning rate
+learning_rate = 0.001
+
 class NeuralNet:
     #variables
-    def __init__(self, Norm_inputs, Norm_outputs):
+    def __init__(self, Norm_inputs, Norm_outputs, learning_rate):
         self.Norm_inputs = Norm_inputs
         self.Norm_outputs = Norm_outputs
+        self.learning_rate = learning_rate
 
         #weights
         self.weights = np.array([ [.50], [.50], [.50] ])
+        self.weights1 = np.array([[0.2, 0.3, 0.2], [0.1, 0.1, 0.1]])
+        self.weights2 = np.array([[0.5, 0.1]])
         self.error_history = []
         self.epoch_list = []
 
@@ -48,14 +54,46 @@ class NeuralNet:
 
     #data will flow through the neural network
     def feed_forward(self):
-        self.hidden = self.sigmoid(np.dot(self.Norm_inputs, self.weights))
+        layer1 = self.sigmoid(np.dot(self.weights1, self.Norm_inputs))
+        layer2 = self.sigmoid(np.dot(self.weights2, layer1))
+        self.hidden = layer2
+        return self.hidden
 
     def backpropagation(self):
-        self.error = self.Norm_outputs - self.hidden
-        delta = self.error * self.sigmoid(self.hidden, deriv=True)
-        self.weights += np.dot(self.Norm_inputs.T, delta)
+        hidden = self.feed_forward()
 
-    def train(self, epochs = 14):
+        self.error = self.Norm_outputs - hidden
+        delta = self.error * self.sigmoid(hidden, deriv=True)
+
+        output_vector1 = np.dot(self.weights1, Norm_inputs)
+        output_vector_hidden = self.sigmoid(output_vector1)
+
+        output_vector2 = np.dot(
+            self.weights2, output_vector_hidden)
+        output_vector_network = self.sigmoid(output_vector2)
+
+        self.weights += np.dot(self.Norm_inputs.T, delta)
+        # update the weights:
+        tmp = self.error * output_vector_network * \
+              (1.0 - output_vector_network)
+
+        tmp = self.learning_rate * np.dot(tmp, output_vector_hidden.T)
+
+        self.weights2 += np.dot
+
+
+        # calculate hidden errors:
+        hidden_errors = np.dot(self.weights2.T, self.error)
+
+        # update the weights:
+        tmp = hidden * output_vector_hidden * \
+              (1.0 - output_vector_hidden)
+        self.weights1 += self.learning_rate * \
+                         np.dot(tmp, Norm_inputs.T)
+
+        return self.weights1, self.weights2
+
+    def train(self, epochs = 2500):
         for epoch in range(epochs):
             #flow forward and produce an output
             self.feed_forward()
@@ -65,25 +103,36 @@ class NeuralNet:
             self.error_history.append(np.average(np.abs(self.error)))
             self.epoch_list.append(epoch)
 
+    def print_matrices(self):
+        print("Layer 1: ", self.weights1)
+        print("Layer 2: ", self.weights2)
+
     #predict function
     def predict(self, new_input):
         prediction = self.sigmoid(np.dot(new_input, self.weights))
         return prediction
 
 #make a neural network
-NN = NeuralNet(Norm_inputs, Norm_outputs)
+NN = NeuralNet(Norm_inputs, Norm_outputs, learning_rate)
 
 #train the network
-example = np.array([[0.2, 0.3, 0.2]])
-example2 = np.array([[0.1, 0.1, 0.1]])
+#example = np.array([[0.2, 0.3, 0.2]])
+#example2 = np.array([[0.1, 0.1, 0.1]])
+
+print("The output: \n")
+updated_weights1 ,updated_weights2 = NN.backpropagation()
+print(updated_weights1, "\n", updated_weights2)
+
+#print(NN.weights2)
+
 
 #predict the correct answers
-print(NN.predict(example), ' - Correct: ', example[0][0])
-print(NN.predict(example2), ' - Correct: ', example2[0][0])
+#print(NN.predict(example), ' - Correct: ', example[0][0])
+#print(NN.predict(example2), ' - Correct: ', example2[0][0])
 
 #plot the error over the entire training
-plt.figure(figsize=(15,5))
-plt.plot(NN.epoch_list, NN.error_history)
-plt.xlabel('Epoch')
-plt.ylabel('Error')
-plt.show()
+#plt.figure(figsize=(15,5))
+#plt.plot(NN.epoch_list, NN.error_history)
+#plt.xlabel('Epoch')
+#plt.ylabel('Error')
+#plt.show()
